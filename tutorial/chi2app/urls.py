164cd5aebf0f58d97a7d7da6ca6e184d9f620de6
heoaa <68603138+heoaa@users.noreply.github.com>
2022-07-21 18:06:32 +0900

    Add files via upload

--- tutorial/chi2app/urls.py
+++ tutorial/chi2app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('list/', views.view_Survey_List),
     path('survey/', views.view_Survey),
     path('insert/', views.set_Survey_Insert),
+    path('analysis/', views.survey_Analysis),
 ]
--- tutorial/chi2app/views.py
+++ tutorial/chi2app/views.py
@@ -63,3 +63,97 @@ def set_Survey_Insert(request):
                     </script>"""
                     
     return HttpResponse(context)
+
+## 설문 분석결과 보기
+def survey_Analysis(request) :
+    
+    ## 설문 데이터 조회하기
+    df = survey.getSurveyList()
+    
+    ## 설문 분석하기(함수로 처리)
+    # rs_df : 분석에 사용된 컬럼 추가된 최종본
+    # rs_ct : 교차표(crossTable) 데이터
+    # rs_msg : 해석 결과
+    rs_df, rs_ct, rs_msg = get_Analysis(df)
+    
+    # 시각화 및 저장하기 (함수로 처리)
+    view_Visualization(rs_df)
+    
+    context = {
+        # 교차표를 html로 전환하여 키 생성
+        'crossTab' : rs_ct.to_html(),
+        # 해석내용
+        'results' : rs_msg
+    }
+    
+    return render(
+        request,
+        'chi2app/analysis.html',
+        context
+    )
+    
+# 분석 및 시각화 함수 v ------------------------------
+import pandas as pd
+import scipy.stats as stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+## 분석하기
+def get_Analysis(df) :
+    # 성별을 숫자로
+    df['genNum'] = df['gender'].apply(lambda g:1 if g=='남' else 2)
+    
+    # 커피숍 이름을 숫자로
+    df['coNum'] = df['co_survey'].apply(lambda c:1 if c=='스타벅스'\
+                                                    else 2 if c=='커피빈'\
+                                                    else 3 if c=='이디야'\
+                                                    else 4)
+    # 교차표(빈도표) 생성
+    crossTab = pd.crosstab(index = df['gender'], columns=df['co_survey'])
+    
+    # 검증하기
+    chi, pv, _, _ = stats.chi2_contingency(crossTab)
+    
+    # 검증결과
+    results = ''
+    
+    if pv > 0.05 :
+        results = 'p-value(유의확률) 값이 <b>{:.3f} > 0.05</b>이므로, '\
+                    '유의수준보다 크게 나타나 귀무가설을 기각할 수 없다. '\
+                    '<br> 따라서, 성별에 따라 선호하는 커피브랜드에는'\
+                    '<b> 차이가 없다고 할 수 있다.(귀무가설 채택)</b>'.format(pv)
+                    
+    else :
+        results = 'p-value(유의확률) 값이 <b>{:.3f} < 0.05</b>이므로, '\
+                    '유의수준보다 작게 나타나 귀무가설을 기각할 수 있다. '\
+                    '<br> 따라서, 성별에 따라 선호하는 커피브랜드에는'\
+                    '<b> 차이가 있다고 할 수 있다.(대립가설 채택)</b>'.format(pv)
+                    
+    return df, crossTab, results
+    
+# 시각화 및 저장하기
+def view_Visualization(df) :
+    # 한글처리
+    plt.rc('font', family='Malgun Gothic')
+    # 그래프 사이즈
+    plt.figure(figsize=(18,18))
+    # fig 객체 얻기 : gcf는  figure와 같음
+    fig = plt.gcf()
+    # 그룹화
+    gen_group = df['co_survey'].groupby(df['coNum']).count()
+    # 그룹에 인덱스 이름 정의하기 (인덱스 번호를 한글 이름으로)
+    gen_group.index = ['스타벅스', '커피빈', '이디야', '탐앤탐스']
+    # 막대그래프 그리기(width = 막대너비)
+    gen_group.plot.bar(color=['red', 'green', 'blue', 'orange'],
+                        width=0.3)
+    # 제목 정의
+    plt.title('커피숍별 선호도', fontsize = 40)
+    plt.xlabel('커피숍', fontsize = 40)
+    plt.ylabel('선호도 건수', fontsize = 40)
+    # 축별 값들에 대한 폰트 사이즈 조정
+    plt.xticks(fontsize = 30, rotation = 0)
+    plt.yticks(fontsize = 30)
+    # 그래프 저장
+    fig.savefig('chi2app/static/chi2app/images/chi2.png') 
+    
+# ---------------------------------------------------
